0e72e5bd28f88c0f6545414e0391ec3091be3daf
juanbcliment <juanbclimentru@gmail.com>
2024-11-20 12:11:03 -0300

    primera carga

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("consulta-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("¡Gracias por tu consulta! Nos pondremos en contacto contigo pronto.");
+});
